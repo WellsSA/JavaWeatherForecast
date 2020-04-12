b5dca78be2d74e01624519b84a4e7c4aca9aeeff
WellsSA <wel.cavzod@gmail.com>
2020-04-12 20:43:15 -0300

    Project setup configuration

---
+++ src/main/java/br/usjt/javaweatherforecast/JavaweatherforecastApplication.java
@@ -0,0 +1,13 @@
+package br.usjt.javaweatherforecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaweatherforecastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaweatherforecastApplication.class, args);
+	}
+
+}
